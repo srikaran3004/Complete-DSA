c2c7f3ec516b5c6760f02a75c08d50520bbf736c
Srikaran <srikaran2230@gmail.com>
2023-11-08 08:26:17 +0530

    DSA

--- Trees/LevelOrderTraversal.cpp
+++ Trees/LevelOrderTraversal.cpp
@@ -1,22 +1,54 @@
-#include<iostream>
+#include <iostream>
+#include <queue>
+
 using namespace std;
 
-struct Node
-{
-    int data;
-    Node *left,*right;
-    Node(int val)
-    {
-        data=val;
-        left=right=NULL;
-    }
+class TreeNode {
+public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int value) : val(value), left(nullptr), right(nullptr) {}
 };
 
-void printLevelOrderLine(Node *root)
-{
-    if(root==NULL)
-    {
-        return ;
+void levelOrderTraversal(TreeNode* root) {
+    if (root == nullptr) {
+        return;
     }
-    
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        TreeNode* current = q.front();
+        q.pop();
+
+        cout << current->val << " ";
+
+        if (current->left) {
+            q.push(current->left);
+        }
+
+        if (current->right) {
+            q.push(current->right);
+        }
+    }
+}
+
+int main() {
+    // Create a sample binary tree
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+
+    // Perform level-order traversal
+    cout << "Level-Order Traversal: ";
+    levelOrderTraversal(root);
+
+    return 0;
 }
